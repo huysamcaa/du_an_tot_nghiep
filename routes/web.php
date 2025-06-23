206e5dec13caed26b9f0be8f094723f7efe40ca2
VuHong2003 <hongvtph52677@gmail.com>
2025-06-23 12:31:48 +0700

    Thông tin người dùng + chỉnh sửa

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\Admin\AdminController;
-use App\Http\Controllers\Admin\AttributeController;
-use App\Http\Controllers\Admin\AttributeValueController;
-use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\ManufacturerController;
-use App\Http\Controllers\Admin\PromotionController;
-use App\Http\Controllers\Admin\ProductController;
-use App\Http\Controllers\Admin\ProductVariantController;
-use App\Http\Controllers\Admin\AdminCartController;
 use App\Http\Controllers\Admin\UserController;
-
-use App\Http\Controllers\Client\HomeController;
-use App\Http\Controllers\Client\CartController;
-use App\Http\Controllers\Client\CheckoutController;
-use App\Http\Controllers\Client\ProductDetailController;
-use App\Http\Controllers\Client\PromotionController as ClientPromotionController;
-use App\Http\Controllers\Client\UserAddressController;
-use App\Http\Controllers\Client\CategoryClientController;
-
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Client\CartController;
+use App\Http\Controllers\Client\HomeController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Admin\CategoryController;
+use App\Http\Controllers\Admin\AdminCartController;
+use App\Http\Controllers\Admin\AttributeController;
+
+use App\Http\Controllers\Admin\PromotionController;
+use App\Http\Controllers\Client\CheckoutController;
+use App\Http\Controllers\Admin\ManufacturerController;
+use App\Http\Controllers\Client\UserAddressController;
+use App\Http\Controllers\Client\UserProfileController;
+use App\Http\Controllers\Admin\AttributeValueController;
+use App\Http\Controllers\Admin\ProductVariantController;
+
+use App\Http\Controllers\Client\ProductDetailController;
+use App\Http\Controllers\Client\CategoryClientController;
+use App\Http\Controllers\Client\PromotionController as ClientPromotionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -87,6 +88,10 @@ Route::middleware(['auth'])->group(function () {
         'destroy' => 'user.addresses.destroy',
     ]);
     Route::post('/my-addresses/{id}/set-default', [UserAddressController::class, 'setDefault'])->name('user.addresses.set_default');
+//// Thông tin người dùng
+Route::get('/profile', [UserProfileController::class, 'show'])->name('client.profile.show');
+Route::get('/profile/edit', [UserProfileController::class, 'edit'])->name('client.profile.edit');
+Route::post('/profile/update', [UserProfileController::class, 'update'])->name('client.profile.update');
 
     /*
     |--------------------------------------------------------------------------
