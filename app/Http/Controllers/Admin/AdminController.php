a101629de5ba9aab8950dd124fa947c4beace9df
ptd98 <ducptph52678@gmail.com>
2025-07-01 10:11:46 +0700

    update nhieu anh sp va thong ke

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,12 +4,18 @@ namespace App\Http\Controllers\Admin;
 use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Shared\Order;
+use App\Models\Admin\Product;
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $revenue = Order::where('is_paid', true)->sum('total_amount');
+        $orderCount = Order::count();
+        $productCount = Product::count();
+        $userCount = User::count();
+
+        return view('admin.dashboard', compact('revenue', 'orderCount', 'productCount', 'userCount'));
     }
     public function manageUsers()
     {
